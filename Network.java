fa56d60788c08e645f3930560f73321338f88f0a
Lior Perlin <lior.perlin@post.runi.ac.il>
2026-01-09 10:45:46 +0200

    Fixed Network toString method (had unnescessary new line)

--- Network.java
+++ Network.java
@@ -137,9 +137,9 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       String outPut = "Network:\n";
+       String outPut = "Network:";
         for(int i = 0; i < this.userCount; i++) { 
-            outPut += this.users[i].toString() + "\n";
+            outPut += "\n" + this.users[i].toString();
         }
        return outPut;
     }
