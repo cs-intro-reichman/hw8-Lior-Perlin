70767e318f2a9f80d78f5796efe3625b40be9b05
Lior Perlin <lior.perlin@post.runi.ac.il>
2026-01-09 09:11:23 +0200

    Finished assaignment

--- Network.java
+++ Network.java
@@ -30,7 +30,14 @@ public class Network {
      *  If there is no such user, returns null.
      *  Notice that the method receives a String, and returns a User object. */
     public User getUser(String name) {
-        //// Replace the following statement with your code
+        // Lets make a loop and go through the network trying to find our name
+        for(int i = 0; i < this.userCount; i++) {
+            // Checks if the user in the current index (using the get name function of User Class) is the same name as the entered name
+            if(this.users[i].getName() == name) {
+                return this.users[i];
+            }
+        }
+        // If we did not return anything by now, that means we ran through the array and did not find the user, so let's return null as requested in such scenario
         return null;
     }
 
@@ -39,42 +46,101 @@ public class Network {
     *  If the given name is already a user in this network, does nothing and returns false;
     *  Otherwise, creates a new user with the given name, adds the user to this network, and returns true. */
     public boolean addUser(String name) {
-        //// Replace the following statement with your code
-        return false;
+        // First let's check if the user already exists or the network is full
+        if(this.getUser(name) != null || userCount >= this.users.length) {
+            return false;
+        }
+        else {
+            User newUser = new User(name);
+            // The current count points to the lowest null index so we can use that
+            users[this.userCount] = newUser;
+            this.userCount++;
+            return true;
+        }
     }
 
     /** Makes the user with name1 follow the user with name2. If successful, returns true.
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        //// Replace the following statement with your code
-        return false;
+        // First let's check if both users exists
+        if(this.getUser(name1) == null || this.getUser(name2) == null) {
+            return false;
+        }
+        else {
+            // Using the addFollowee function of User Class. if it failed than it means name1's follows array is full...
+            return this.getUser(name1).addFollowee(name2);
+        }
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
      *  the user that has the maximal mutual number of followees as the user with the given name. */
     public String recommendWhoToFollow(String name) {
-        //// Replace the following statement with your code
-        return null;
+        // Making our lives easy by making a specific user to point on the user's name
+        User cur = this.getUser(name);
+        // Let's make sure the user actually exist
+        if(cur == null) {
+            return null;
+        }
+        // If both instensence were false that means we are good to find a recommended new followee
+        else {
+            // Let's create veriables to remember the most recommended to follow and the amount of friends they have in common
+            // we will see how many common friends they have using User Class countMutual function
+            // Most mutual starts at -1 because if someone doesn't has friends at all or noone has a mutual friend with him we still want to recommend someone.
+            String mostCommonName = null;
+            int mostMutualAmount = -1;
+            for(int i = 0; i < this.userCount; i++) {
+                // Let's make sure our we skip our inputted user
+                if(this.users[i] == cur) {
+                    i++;
+                }
+                // Now check if this user has more mutual friends than the one we had most
+                if(this.users[i].countMutual(cur) > mostMutualAmount) {
+                    // If so, lets remember how many mutual friends the one with the most had and the person that our user had the most mutual friends with
+                    mostMutualAmount = this.users[i].countMutual(cur);
+                    mostCommonName = this.users[i].getName();
+                }
+            }
+            return mostCommonName;
+        }
     }
 
     /** Computes and returns the name of the most popular user in this network: 
      *  The user who appears the most in the follow lists of all the users. */
     public String mostPopularUser() {
-        //// Replace the following statement with your code
-        return null;
+        // Make sure the network has users
+        if(this.userCount <= 0) {
+            return null;
+        }
+        int mostPopularAmount = -1;
+        String mostPopularName = "";
+        for(int i = 0; i < this.userCount; i++) { 
+            if(followeeCount(this.users[i].getName()) > mostPopularAmount) {
+                mostPopularAmount = followeeCount(this.users[i].getName());
+                mostPopularName = this.users[i].getName();
+            }
+        }
+        return mostPopularName;
     }
 
     /** Returns the number of times that the given name appears in the follows lists of all
      *  the users in this network. Note: A name can appear 0 or 1 times in each list. */
     private int followeeCount(String name) {
-        //// Replace the following statement with your code
-        return 0;
+        int counter = 0;
+        for(int i = 0; i < this.userCount; i++) { 
+            if(this.users[i].follows(name)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-       //// Replace the following statement with your code
-       return null;
+       String outPut = "";
+        for(int i = 0; i < this.userCount; i++) { 
+            outPut += this.users[i].toString() + "\n";
+        }
+       return outPut;
     }
 }
--- User.java
+++ User.java
@@ -43,36 +43,78 @@
 
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
-        //// Replace the following statement with your code
+        // Goes though the loop the amount of follow count there are, and checks if the name is there
+        for(int i = 0; i < fCount; i++) {
+            if(follows[i] == name) {
+                return true;
+            }
+        }
+        // If we went through the entire counted followees and didn't return true,  that means the user does not follow the given name so give back false
         return false;
     }
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
-        //// Replace the following statement with your code
-        return false;
+        // Check if the follow list is full or the user already follows the person
+        if(this.fCount == 10 || this.follows(name)) {
+            return false;
+        }
+        // If the follow list is not full
+        else {
+            // Add the name the the fCount position (which is always one higher then the last person added) and make sure the count is higher
+            this.follows[this.fCount] = name;
+            this.fCount++;
+            return true;
+        }
     }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
-        //// Replace the following statement with your code
+        for(int i = 0; i < this.fCount; i++) {
+            // Checks if the name exists
+            if(follows(this.follows[i])) {
+                // Let's bring every person a step back on the array
+                while(i < this.fCount - 1) {
+                    // Make this current follows the one after
+                    this.follows[i] = this.follows[i + 1];
+                    // Make the next follows null
+                    this.follows[i + 1] = null;
+                    i++;
+                }
+                // After moving all followees in the array one step back let's get the counter one back and return false
+                this.fCount--;
+                return true;
+            }
+        }
+        // If we ran through the entire array and didn't return true, that means we did not find the followee on the array so we'll return false
         return false;
     }
 
     /** Counts the number of users that both this user and the other user follow.
     /*  Notice: This is the size of the intersection of the two follows lists. */
     public int countMutual(User other) {
-         //// Replace the following statement with your code
-        return 0;
+        // Make sure the inputted user is not null
+        if(other == null) {
+            return 0;
+        }
+        int counter = 0;
+        for(int i = 0; i < this.fCount; i++) {
+            // At every index of our user's follows array, lets check if he exist's in the others users array using "follows" function.
+            if(other.follows(this.follows[i])) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     /** Checks is this user is a friend of the other user.
      *  (if two users follow each other, they are said to be "friends.") */
     public boolean isFriendOf(User other) {
-        //// Replace the following statement with your code
-        return false;
+        // Checks usingn the follows function of the current user's name exist on the other users followees name.
+        return other.follows(this.name);
     }
+
     /** Returns this user's name, and the names that s/he follows. */
     public String toString() {
         String ans = name + " -> ";
